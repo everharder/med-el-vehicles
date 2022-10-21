ed7a89ed8ac7802d93cacd2d02bf1f53067c9bf6
Daniel Eberharter <daniel.eberharter@student.uibk.ac.at>
2022-10-21 22:43:25 +0200

    add configuration classes

--- MedEl.Vehicles/MedEl.Vehicles.CLI/Program.cs
+++ MedEl.Vehicles/MedEl.Vehicles.CLI/Program.cs
@@ -1,7 +1,15 @@
-﻿using System.ComponentModel.Design;
+﻿using MedEl.Vehicles.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello World");
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureHostConfiguration((configBuilder) => configBuilder.AddJsonFile("appsettings.json"))
+    .ConfigureServices((_, services) =>
+        services.AddRepository())
+    .Build();
 
-
+RepositoryConfiguration config = host.Services.GetRequiredService<RepositoryConfiguration>();
+Console.WriteLine(config.UseCaching);
 
 Console.Read();
---
+++ MedEl.Vehicles/MedEl.Vehicles.Common/Configuration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedEl.Vehicles.Common
+{
+    /// <summary>
+    /// Common base class for all configurations
+    /// Allows to access optional appsettings.json config values and use fallback values if desired
+    /// </summary>
+    public abstract class BaseConfiguration
+    {
+        private readonly IConfiguration configuration;
+
+        /// <summary>
+        /// Creates a new instance of the base configuration
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public BaseConfiguration(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        protected virtual string settingPrefix => string.Empty;
+
+        protected TValue? getConfig<TValue>(TValue? defaultValue = default(TValue?), [CallerMemberName] string? settingName = null)
+        {
+            if(string.IsNullOrWhiteSpace(settingName))
+            {
+                throw new ArgumentNullException(nameof(settingName));
+            }
+
+            string prefix = settingPrefix;
+            if(!string.IsNullOrWhiteSpace(prefix) && !prefix.EndsWith(":"))
+            {
+                prefix += ":";
+            }
+
+            string settingValue = configuration[$"{prefix}{settingName}"];
+            if(string.IsNullOrWhiteSpace(settingValue))
+            {
+                return defaultValue;
+            }
+            return (TValue)Convert.ChangeType(settingValue, typeof(TValue));
+        }
+    }
+}
